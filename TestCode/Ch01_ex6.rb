12690d62956df07249527c4011c42f515eec7a35
SStoeckigt <sureka.stoeckigt@gmail.com>
2019-01-06 15:25:16 +0000

    new example

--- TestCode/Ch01_ex6.rb
+++ TestCode/Ch01_ex6.rb
@@ -1,5 +1,36 @@
-# given a string, return the character after every letter "r"
+#You'll get a string and a boolean.
+# When the boolean is true, return a new string containing all the odd characters.
+# When the boolean is false, return a new string containing all the even characters.
 #
-# pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
-# pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
-# pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
+# If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
+#
+# odds_and_evens("abcdefg",true)    # => "bdf"
+# odds_and_evens("abcdefg",false)   # => "aceg"
+=begin
+def odds_and_evens(string,bool)
+ string.each do |char|
+    if bool == true
+     if string.index(char)%2 ==0
+        return string[char]
+     else
+        return ''
+     end
+        #return string[(string.index(char)%2 == 0)]
+    else
+        string.index(char)%2 !=0
+        return string[char]
+    else
+        return ''
+
+    end
+end
+end
+=end
+
+def odds_and_evens(string,boo)
+    if boo == true    
+        string.each{|char| puts char if string[char+1].even?}
+    else
+        string.each{|char| puts char if string[char+1].odd?}
+    end
+end
